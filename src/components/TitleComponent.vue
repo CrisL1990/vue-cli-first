487aeee8af27ca09842b3ff92d9120389cb75e9d
CrisL1990 <lucchetta.cris@gmail.com>
2022-02-18 18:01:20 +0100

    Esercizio completato

---
+++ src/components/TitleComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>La mia prima app con Vue!</h2>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'TitleComponent'
+    }
+</script>
+
+<style scoped>
+
+</style>
